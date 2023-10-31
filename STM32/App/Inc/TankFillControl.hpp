7f0991b9044ea34e188a59af89d126d8329c3e6f
Daniel Burger <burger.daniel@gmx.net>
2023-10-31 10:16:22 +0100

    Implemented tank fill control

--- STM32/App/Inc/TankFillControl.hpp
+++ STM32/App/Inc/TankFillControl.hpp
@@ -8,9 +8,31 @@
 #ifndef SRC_TANKFILLCONTROL_HPP_
 #define SRC_TANKFILLCONTROL_HPP_
 
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "DO24V.hpp"
+#include "DI24V.hpp"
+#include "commands.h"
+
+typedef enum {
+	INIT, FILL_SMALL, FILL_LARGE, DRAIN
+} FsmStates_t;
+
 class TankFillControl {
+private:
+	FsmStates_t state;
+	QueueHandle_t commandQueue;
+	DO_24V &valveSmallTankInlet;
+	DO_24V &valveLargeTankInlet;
+	DI_24V &switchSmallTankFull;
+	DI_24V &switchLargeTankFull;
+
 public:
-	TankFillControl();
+	TankFillControl(QueueHandle_t cmdQueue, DO_24V &vSmallTankInlet,
+			DO_24V &vLargeTankInlet, DI_24V &swSmallTankFull,
+			DI_24V &swLargeTankFull);
+
+	void run();
 };
 
 #endif /* SRC_TANKFILLCONTROL_HPP_ */
--- STM32/App/Src/TankFillControl.cpp
+++ STM32/App/Src/TankFillControl.cpp
@@ -7,8 +7,76 @@
 
 #include "TankFillControl.hpp"
 
-TankFillControl::TankFillControl() {
-	// TODO Auto-generated constructor stub
-
+TankFillControl::TankFillControl(QueueHandle_t cmdQueue,
+		DO_24V &vSmallTankInlet,
+		DO_24V &vLargeTankInlet,
+		DI_24V &swSmallTankFull,
+		DI_24V &swLargeTankFull) :
+		commandQueue(cmdQueue),
+		valveSmallTankInlet(vSmallTankInlet),
+		valveLargeTankInlet(vLargeTankInlet),
+		switchSmallTankFull(swSmallTankFull),
+		switchLargeTankFull(swLargeTankFull) {
+	state = INIT;
 }
 
+void TankFillControl::run() {
+	// Get command received from high level controller
+	Commands_t command = NONE;
+	xQueueReceive(commandQueue, &command, (TickType_t) 0);
+
+	// FSM
+	switch (state) {
+	case INIT:
+		valveLargeTankInlet.setOff();
+		valveSmallTankInlet.setOff();
+
+		if (command == FILL_LARGE_TANK) {
+			state = FILL_LARGE;
+		} else if (command == FILL_NO_TANK) {
+			state = DRAIN;
+		} else {
+			state = FILL_SMALL;
+		}
+		break;
+	case FILL_SMALL:
+		if (switchSmallTankFull.read()) {
+			valveSmallTankInlet.setOff();
+		} else {
+			valveSmallTankInlet.setOn();
+		}
+
+		if (command == FILL_LARGE_TANK) {
+			state = FILL_LARGE;
+		} else if (command == FILL_NO_TANK) {
+			state = DRAIN;
+		}
+		break;
+	case FILL_LARGE:
+		if (switchLargeTankFull.read()) {
+			valveLargeTankInlet.setOff();
+		} else {
+			valveLargeTankInlet.setOn();
+		}
+
+		if (command == FILL_SMALL_TANK) {
+			state = FILL_SMALL;
+		} else if (command == FILL_NO_TANK) {
+			state = DRAIN;
+		}
+		break;
+	case DRAIN:
+		valveLargeTankInlet.setOff();
+		valveSmallTankInlet.setOff();
+
+		if (command == FILL_SMALL_TANK) {
+			state = FILL_SMALL;
+		} else if (command == FILL_LARGE_TANK) {
+			state = FILL_LARGE;
+		}
+		break;
+	default:
+		state = INIT;
+		break;
+	}
+}
