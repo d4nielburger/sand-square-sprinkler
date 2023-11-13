fa1a3e61afc7e5e6e1674e6e8cb88dbf25a7805c
Daniel Burger <burger.daniel@gmx.net>
2023-11-13 22:38:29 +0100

    test buttons for all controlls

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,28 @@
 <script setup>
 import GaragePumpControl from './components/GaragePumpControl.vue';
+import TankFillSelection from './components/TankFillSelection.vue';
+import SprinklerControl from './components/SprinklerControl.vue';
+import HoseControl from './components/HoseControl.vue';
 import StatusDisplay from './components/StatusDisplay.vue';
 </script>
 
 <template>
   <div id="app">
-    <StatusDisplay />
-    <GaragePumpControl />
+    <div class="main-element">
+      <StatusDisplay/>
+    </div>
+    <div id="control" class="main-element">
+      <h2>Controls</h2>
+      <GaragePumpControl/>
+      <TankFillSelection/>
+      <SprinklerControl/>
+      <HoseControl/>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.main-element{
+  padding: 2em;
+}
 </style>
---
+++ client/src/components/TankFillSelection.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+      <h3>Tank Fill Selection</h3>
+      <button @click="selectSmall">small</button>
+      <button @click="selectLarge">large</button>
+      <button @click="selectNone">drain</button>
+    </div>
+  </template>
+  
+  <script>
+  const API_URI = 'http://localhost:3000/sss/api';
+  
+  export default {
+    methods: {
+      async selectSmall() {
+        try {
+          await fetch(`${API_URI}/control/tank`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ select: 'small' }),
+          });
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      },
+      async selectLarge() {
+        try {
+          await fetch(`${API_URI}/control/tank`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ select: 'large' }),
+          });
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      },
+      async selectNone() {
+        try {
+          await fetch(`${API_URI}/control/tank`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ select: 'none' }),
+          });
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Your CSS here */
+  </style>
+  
