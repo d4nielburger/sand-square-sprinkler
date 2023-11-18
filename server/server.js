d9abd7795349df217565e051a6f853849da7f3c9
Daniel Burger <burger.daniel@gmx.net>
2023-11-18 23:01:13 +0100

    Removed testing stuff

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const socketIO = require('socket.io');
+
 // Config
 const PORT = 3000;
 const API_URI = '/sss/api'
@@ -35,15 +36,6 @@ app.use((_, res, next) => {
 });
 
 app.get(`${API_URI}/status`, async (req, res) => {
-/*   try {
-    const data = await fs.readFile('status.json', 'utf8');
-    const status = JSON.parse(data);
-    res.status(200).json(status);
-  } catch (error) {
-    console.error("Error reading status.json:", error);
-    res.status(500).send('Server error');
-  } */
-
    res.status(200).json(status);
 });
 
@@ -53,33 +45,5 @@ app.use(`${API_URI}/control`, controlRoutes);
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
-// For testing
-async function updateStatus(key, newState) {
-  try {
-    const data = await fs.readFile('status.json', 'utf8');
-    const statuses = JSON.parse(data);
-
-    statuses[key] = newState;
-
-    await fs.writeFile('status.json', JSON.stringify(statuses, null, 2), 'utf8');
-    console.log(`Status updated for ${key}`);
-
-    io.emit('status_update_available');
-
-    return true; // success
-  } catch (error) {
-    console.error(`Error updating the status for ${key}:`, error);
-    return false; // failure
-  }
-}
-
-function sendGenericResponse(res,success) {
-  if (success) {
-    res.status(200).json({ message: "Request successful" });
-  } else {
-    res.status(500).send('Server error');
-  }  
-}
 
 module.exports = io;
