f80b9f348719c11344c17e33c6a85539f0f0ae67
Daniel Bachmann <dany.bachi@googlemail.com>
2023-11-18 17:43:42 +0100

    changed to one button per control and added colors

--- client/src/components/GaragePumpControl.vue
+++ client/src/components/GaragePumpControl.vue
@@ -1,40 +1,68 @@
 <template>
   <div>
     <h3>Garage Pump Control</h3>
-    <button class="control-btn" @click="pumpOn">Pump On</button>
-    <button class="control-btn" @click="pumpOff">Pump Off</button>
+    <button  :class="{ 'control-btn-green': status.garagePump === 'on', 'control-btn-red': status.garagePump === 'off' }" @click="togglePump">
+      {{ status.garagePump === 'on' ? 'Turn OFF Garage Pump' : 'Turn ON Garage Pump' }}
+    </button>
   </div>
 </template>
 
 <script>
 const API_URI = 'http://localhost:3000/sss/api';
+import io from 'socket.io-client';
 
 export default {
-  methods: {
-    async pumpOn() {
-      try {
-        await fetch(`${API_URI}/control/garage-pump`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ state: 'on' }),
+  data() {
+        return {
+            status: {},
+        }
+    },
+    mounted() {
+        this.fetchStatus();
+
+        const socket = io('http://localhost:3000');
+        socket.on('status_update_available', () => {
+            this.fetchStatus();
         });
-      } catch (error) {
-        console.error('Error:', error);
+    },
+  methods: {
+    async togglePump() {
+      if (this.status.garagePump == 'on') {
+        try {
+          await fetch(`${API_URI}/control/garage-pump`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ state: 'off' }),
+          });
+        } catch (error) {
+          console.error('Error:', error);
+        }        
+      } else {
+        try {
+          await fetch(`${API_URI}/control/garage-pump`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ state: 'on' }),
+          });
+        } catch (error) {
+          console.error('Error:', error);
+        }        
       }
     },
-    async pumpOff() {
+    async fetchStatus() {
       try {
-        await fetch(`${API_URI}/control/garage-pump`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ state: 'off' }),
-        });
+          const response = await fetch('http://localhost:3000/sss/api/status');
+          if (response.ok) {
+              this.status = await response.json();
+          } else {
+              console.error('Failed to get status');
+          }
       } catch (error) {
-        console.error('Error:', error);
+          console.error('Error fetching status:', error);
       }
     },
   },
--- client/src/components/TankFillSelection.vue
+++ client/src/components/TankFillSelection.vue
@@ -1,16 +1,36 @@
 <template>
     <div>
       <h3>Tank Fill Selection</h3>
-      <button class="control-btn" @click="selectSmall">small</button>
-      <button class="control-btn" @click="selectLarge">large</button>
-      <button class="control-btn" @click="selectNone">drain</button>
+      <button :class="{ 'control-btn-green': status.tankSelect === 'small', 'control-btn': status.tankSelect === 'none' || 'large' }" @click="selectSmall">
+        small
+      </button>
+      <button :class="{ 'control-btn-green': status.tankSelect === 'large', 'control-btn': status.tankSelect === 'none' || 'small' }" @click="selectLarge">
+        large
+      </button>
+      <button :class="{ 'control-btn-green': status.tankSelect === 'none', 'control-btn': status.tankSelect === 'small' || 'large' }" @click="selectNone">
+        drain
+      </button>
     </div>
   </template>
   
   <script>
   const API_URI = 'http://localhost:3000/sss/api';
+  import io from 'socket.io-client';
   
   export default {
+    data() {
+        return {
+            status: {},
+        }
+    },
+    mounted() {
+        this.fetchStatus();
+
+        const socket = io('http://localhost:3000');
+        socket.on('status_update_available', () => {
+            this.fetchStatus();
+        });
+    },
     methods: {
       async selectSmall() {
         try {
@@ -49,6 +69,18 @@
           });
         } catch (error) {
           console.error('Error:', error);
+        }
+      },
+      async fetchStatus() {
+        try {
+            const response = await fetch('http://localhost:3000/sss/api/status');
+            if (response.ok) {
+                this.status = await response.json();
+            } else {
+                console.error('Failed to get status');
+            }
+        } catch (error) {
+            console.error('Error fetching status:', error);
         }
       },
     },
